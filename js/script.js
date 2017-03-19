112d535812c434953afd7b0b4e405d8c2d12a1d2
lmitchell4 <lmitchell4@gmail.com>
2017-03-19 12:38:27 -0700

    Switch to five cats

--- js/script.js
+++ js/script.js
@@ -1,43 +1,101 @@
 
-
-var catOneName = "Chloe's";
-$("#cat-1-name").text(catOneName);
-var counter1 = 0;
-$("#cat-1").click(function(e) {
-  counter1++;
-  $("#count-1").text(counter1);
-});
-
-var catTwoName = "Kitty's";
-$("#cat-2-name").text(catTwoName);
-var counter2 = 0;
-$("#cat-2").click(function(e) {
-  counter2++;
-  $("#count-2").text(counter2);
-});
+var numOfCats = 5;
+var catNames = ["Chloe","Whiskers","Fred","Sprite","Jack and Jill"];
+var catPics = ["cat-1-300.png","cat-2-300.png","cat-3-250.png",
+               "cat-4-300.png","cat-5-300.png"];
+var counters = [];
+var currentCat;
 
 
-var nums = [1,2,3];
+function createCat(num) {
+  var cat = "<div id='cat'>";
+  cat += "<h2>" + catNames[num] + "</h2>";
+  cat += "<a href='#'><img id='cat-img-" + num + "' src='images/" + catPics[num] + "'></a>";
+  cat += "<div>";
+  cat += "<h2 class='inline'></h2>";
+  cat += "<h2 class='inline'> clicks:</h2>";
+  cat += "<h2 id='cat-counter-" + num + "' class='inline'>" + counters[num] + "</h2>";
+  cat += "</div>";
+  cat += "</div>";
+  return cat;
+}
 
-// Let's loop over the numbers in our array
-for (var i = 0; i < nums.length; i++) {
+// var catList = [];
+var catList = $("#cat-list");
+for(var i = 0; i < numOfCats; i++) {
+  var item = "<li>";
+  item += "<a id='cat-name-" + i + "' href='#' class='cat-name'>";
+  item += catNames[i];
+  item += "</a>";
+  item += "</li>";
+  catList.append(item);
 
-  // This is the number we're on...
-  var num = nums[i];
+  // Set up the counter for this cat:
+  counters[i] = 0;
+  
+  // Display the selected cat:
+  $("#cat-name-" + i).click(function(catNum) {
+    return function() {
+      // Create an element for this cat:
+      var catElem = createCat(catNum);
 
-  // We're creating a DOM element for the number
-  var elem = document.createElement('div');
-  elem.textContent = num;
+      if(catNum != currentCat) {
+        currentCat = catNum;
+        $("#cat").remove();
+        $("#cat-container").append(catElem);
+      }
 
-  // ... and when we click, alert the value of `num`
-  elem.addEventListener('click', (function(numCopy) {
-      return function() {
-          alert(numCopy);
-      };
-  })(num));
+      // Keep track of clicks:
+      $("#cat-img-" + catNum).click(function(catNum) {
+        return function() {
+          counters[catNum]++;
+          console.log("ok");
+          $("#cat-counter-" + catNum).text(counters[catNum]);
+        };
+      }(catNum));
 
-  document.body.appendChild(elem);
-};
+    }
+  }(i));
+}
+
+// var catOneName = "Chloe's";
+// $("#cat-1-name").text(catOneName);
+// var counter1 = 0;
+// $("#cat-1").click(function(e) {
+  // counter1++;
+  // $("#count-1").text(counter1);
+// });
+
+// var catTwoName = "Kitty's";
+// $("#cat-2-name").text(catTwoName);
+// var counter2 = 0;
+// $("#cat-2").click(function(e) {
+  // counter2++;
+  // $("#count-2").text(counter2);
+// });
+
+
+// var nums = [1,2,3];
+
+// // Let's loop over the numbers in our array
+// for (var i = 0; i < nums.length; i++) {
+
+  // // This is the number we're on...
+  // var num = nums[i];
+
+  // // We're creating a DOM element for the number
+  // var elem = document.createElement('div');
+  // elem.textContent = num;
+
+  // // ... and when we click, alert the value of `num`
+  // elem.addEventListener('click', (function(numCopy) {
+      // return function() {
+          // alert(numCopy);
+      // };
+  // })(num));
+
+  // document.body.appendChild(elem);
+// };
 
 
 
@@ -109,7 +167,7 @@ for (var i = 0; i < nums.length; i++) {
     // var wikiRequestTimeout = setTimeout(function() {
       // $wikiElem.text("Failed to get Wikipedia resources");
     // }, 8000);
-    
+
     // $.ajax({
       // url: "http://en.wikipedia.org/w/api.php",
       // data: {
