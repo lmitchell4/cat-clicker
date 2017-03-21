0da21e56b38a8428bf2ba7b5bea0f4a73adc67f5
lmitchell4 <lmitchell4@gmail.com>
2017-03-20 17:52:03 -0700

    chore: Clean up commented out code from previous project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,89 @@
+$(function(){
+  // Hard-coding the data:
+  catData = 
+  numOfCats = 5;
+  catNames = ["Chloe","Whiskers","Fred","Sprite","Jack and Jill"];
+  catPics = ["cat-1-300.png","cat-2-300.png","cat-3-250.png",
+                 "cat-4-300.png","cat-5-300.png"];
+  counters = [];
+  currentCat;
+
+  var model = {
+    
+    
+    init: function() {
+      if (!localStorage.notes) {
+        localStorage.notes = JSON.stringify([]);
+      }
+    },
+    add: function(obj) {
+      var data = JSON.parse(localStorage.notes);
+      data.push(obj);
+      localStorage.notes = JSON.stringify(data);
+    },
+    getAllNotes: function() {
+      return JSON.parse(localStorage.notes);
+    }
+  };
+
+
+  var octopus = {
+    addNewNote: function(noteStr) {
+      model.add({
+        
+
+        
+        // cat name, clickCount, image url
+        content: noteStr,
+        dateSubmitted: Date.now()
+      });
+      view.render();
+    },
+
+    getNotes: function() {
+      return model.getAllNotes();
+      // return model.getAllNotes().reverse();
+    },
+
+    init: function() {
+      model.init();
+      view.init();
+    }
+  };
+
+
+  var view = {
+    init: function() {
+      this.noteList = $('#notes');
+      var newNoteForm = $('#new-note-form');
+      var newNoteContent = $('#new-note-content');
+      newNoteForm.submit(function(e){
+          octopus.addNewNote(newNoteContent.val());
+          newNoteContent.val('');
+          e.preventDefault();
+      });
+      view.render();
+    },
+    render: function(){
+      var htmlStr = '';
+      octopus.getNotes().forEach(function(note){
+            htmlStr += '<li class="note">';
+            htmlStr += '<span class="note-date">';
+            htmlStr += new Date(note.dateSubmitted).toString();
+            htmlStr += '</span>';
+            htmlStr += note.content + '</li>';
+      });
+      this.noteList.html( htmlStr );
+    }
+  };
+
+  octopus.init();
+});
+
+
+
+
+
 
 var numOfCats = 5;
 var catNames = ["Chloe","Whiskers","Fred","Sprite","Jack and Jill"];
@@ -96,111 +182,3 @@ for(var i = 0; i < numOfCats; i++) {
 
   // document.body.appendChild(elem);
 // };
-
-
-
-
-
-// function loadData() {
-
-    // var $body = $("body");
-    // var $wikiHeaderElem = $("#wikipedia-header");
-    // var $wikiElem = $("#wikipedia-links");
-    // var $nytHeaderElem = $("#nytimes-header");
-    // var $nytElem = $("#nytimes-articles");
-    // var $greeting = $("#greeting");
-
-    // // clear out old data before new request
-    // $wikiElem.text("");
-    // $nytElem.text("");
-
-    // // load streetview
-    // var streetStr = $("#street").val();
-    // var cityStr = $("#city").val();
-    // var address = streetStr + "," + cityStr;
-
-    // $greeting.text("So, you want to live at " + address + "?");
-
-    // streetViewStr = '<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=';
-    // streetViewStr += address + '">';
-    // $body.append(streetViewStr);
-
-
-    // // // load NY Times articles:
-    // // var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    // // nytUrl += "?" + $.param({
-      // // "api-key": nytimes_api_key
-    // // });
-
-    // // $.getJSON(nytUrl, function(data) {
-
-      // // $nytHeaderElem.text("New York Times Articles About " + cityStr);
-
-      // // var articles = data.response.docs;
-      // // var items = [];
-      // // $.each(articles, function(key, article) {
-        // // var article_item = "<li class='article'>";
-        // // article_item += "<a href='" + article.web_url + "'>";
-        // // article_item += article.headline.main;
-        // // article_item += "</a>";
-        // // article_item += "<p>" + article.snippet + "</p>";
-        // // article_item += "</li>";
-        // // items.push(article_item);
-      // // });
-
-      // // var final_list = items.join("");
-      // // $nytElem.append(final_list);
-
-      // // // $("<ul/>", {
-        // // // // "id": "nytimes-articles",
-        // // // "class": "my-new-list",
-        // // // html: items.join("")
-      // // // }).appendTo($("#nytimes-articles"));
-
-    // // }).done(function() {
-    // // }).fail(function() {
-        // // $nytHeaderElem.text("New York Times Articles Could Not Be Loaded");
-    // // });
-
-
-    // // load Wikipedia articles:
-    // var wikiRequestTimeout = setTimeout(function() {
-      // $wikiElem.text("Failed to get Wikipedia resources");
-    // }, 8000);
-
-    // $.ajax({
-      // url: "http://en.wikipedia.org/w/api.php",
-      // data: {
-        // action: "query",
-        // list: "search",
-        // srsearch: cityStr,
-        // format: "json"
-      // },
-      // dataType: "jsonp",
-      // // jsonp: "callback",
-      // success: function (data) {
-
-        // $wikiHeaderElem.text("Wikipedia Articles About " + cityStr);
-        // var articles = data.query.search;
-        // console.log(data);
-
-        // var items = [];
-        // $.each(articles, function(key, article) {
-          // url = "http://en.wikipedia.org/wiki/" + article.title;
-          // var article_item = "<a href='" + url + "'>";
-          // article_item += article.title;
-          // article_item += "</a>";
-          // items.push(article_item);
-        // });
-
-        // var final_list = items.join("");
-        // $wikiElem.append(final_list);
-
-        // clearTimeout(wikiRequestTimeout);
-      // }
-    // });
-
-  // return false;
-// };
-// // $('#form-container').submit(loadData);
-// $('#submit-btn').click(loadData);
